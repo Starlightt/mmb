0b7d4168aafa1d8fdf41cd83eaab552689000830
Starlightt <31811940+Starlightt@users.noreply.github.com>
2017-09-12 12:04:27 +0530

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -18,28 +18,7 @@
  */
 var app = {
 
-       admob.createBannerView({
-    autoShowBanner: true
-});
-    admob.requestInterstitialAd({
-    autoShowInterstitial: true
-});
-    var isPendingInterstitial = false;
-var isAutoshowInterstitial = false;
-
-function prepareInterstitialAd() {
-    if (!isPendingInterstitial) { // We won't ask for another interstitial ad if we already have an available one
-        admob.requestInterstitialAd({
-            autoShowInterstitial: isAutoshowInterstitial
-        });
-    }
-}
-
-function onAdLoadedEvent(e) {
-    if (e.adType === admob.AD_TYPE.INTERSTITIAL && !isAutoshowInterstitial) {
-        isPendingInterstitial = true;
-    }
-}
+ 
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -56,36 +35,10 @@ function onAdLoadedEvent(e) {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        document.removeEventListener('deviceready', onDeviceReady, false);
-
-    admob.setOptions({
-        publisherId:          "ca-app-pub-2795428358472042~1387971351",
-        interstitialAdId:     "ca-app-pub-2795428358472042/8883317996",
-    });
-
-    document.addEventListener(admob.events.onAdLoaded, onAdLoadedEvent);
-    prepareIntestitialAd();
 
         app.receivedEvent('deviceready');
         
-    }
-    document.addEventListener("deviceready", onDeviceReady, false);
-
-function showInterstitialAd() {
-    if (isPendingInterstitial) {
-        admob.showInterstitialAd(function () {
-                isPendingInterstitial = false;
-                isAutoshowInterstitial = false;
-                prepareInterstitialAd();
-        });
-    } else {
-        // The interstitial is not prepared, so in this case, we want to show the interstitial as soon as possible
-        isAutoshowInterstitial = true;
-        admob.requestInterstitialAd({
-            autoShowInterstitial: isAutoshowInterstitial
-        });
-    }
-}
+    },
     // Update DOM on a Received Event
     
     receivedEvent: function(id) {
